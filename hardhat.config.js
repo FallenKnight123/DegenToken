b933b7b6914d80bcead2d04d1777b62e79007870
FallenKnight123 <8215291@ntc.edu.ph>
2024-05-16 10:56:48 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
-
-const ALCHEMY_API_KEY = "MxRkQz6DXFRfElXTJhX_A40niWy5HpAZ";
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: {
-    version: "0.8.20", // Update this to match the Solidity version used by OpenZeppelin contracts
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
+  solidity: "0.8.0",
   networks: {
-    ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: []
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      gas: 8000000, // Adjust the gas limit as needed
     }
   }
 };
